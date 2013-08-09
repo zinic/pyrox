4665dea64f138cf7f6db84eb4687636fc46c050e
Chad Lung <chad.lung@gmail.com>
2013-08-08 20:02:33 -0500

    Log when config is loaded, Add comment to SimpleFilter

--- pyrox/stock_filters/simple.py
+++ pyrox/stock_filters/simple.py
@@ -2,7 +2,10 @@ from pyrox.http.filtering import HttpFilter, pass_event
 
 
 class SimpleFilter(HttpFilter):
-
+    """
+    This is an example of a simple filter that simply prints out the
+    user-agent value from the header
+    """
     def on_request(self, request_message):
         user_agent_header = request_message.get_header('user-agent')
         if user_agent_header and len(user_agent_header.values) > 0:
